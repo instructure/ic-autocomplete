dd7299750be3fd00492bf4e8b2fef8da99d91e01
Ryan Florence <rpflorence@gmail.com>
2014-05-01 11:12:16 -0600

    basic "render" test, more coming soon

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,7 +1,18 @@
 moduleForComponent('ic-autocomplete');
 
 test('renders', function() {
-  expect(1);
-  ok("I'll write some tests");
+  expect(2);
+  var modal = this.subject({
+    template: function() {/*
+      {{#ic-autocomplete}}
+        {{#ic-autocomplete-option value="hola" label="Hello"}}
+          Hello
+        {{/ic-autocomplete-option}}
+      {{/ic-autocomplete}}
+    */}
+  });
+  equal(modal.state, 'preRender');
+  this.append();
+  equal(modal.state, 'inDOM');
 });
 
